0405061485ca7fd4596b9c0ef66ebd86f1668deb
Gus86git <gus.gjb8@gmail.com>
2026-03-09 15:54:58 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@
 // ¡REEMPLAZA ESTA URL CON LA DE TU STREAM REAL DE AUDIO!
 // Ejemplo: 'https://tuserver.com:8000/live.mp3'
 // Esta URL te la da tu servidor de streaming (Icecast/Shoutcast)
-const STREAM_URL = 'https://icecast.radiofrance.fr/francemusique-midfi.mp3';
+const STREAM_URL = 'https://streaming1.hostingmontevideo.com:7019/';
 // ============================================================
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -158,6 +158,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
 
 
 
